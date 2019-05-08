e97595d356fdb1dd8a0c7753cb224c592f895c36
VinceMar <gc_qq744196568@126.com>
2019-05-08 19:17:15 +0800

    加了返回顶部按钮

--- js/js.js
+++ js/js.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-  $("#name").hover(function(){
+$(document).ready(function() {
+  $("#name").hover(function() {
     $("#name").addClass("animated bounce infinite");
-  },function(){
+  }, function() {
     $("#name").removeClass("animated bounce infinite");
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 40) {
+      $("#top").fadeIn();
+    } else {
+      $("#top").fadeOut();
+    }
+  });
+
+  $("#top").click(function() {
+    $("html,body").animate({
+      scrollTop: 0
+    }, 800);
   });
 });
